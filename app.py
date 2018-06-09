34546747ee75bab8c635aae4223f8d7041749a72
Rick Pospisil <rick.posp@gmail.com>
2018-06-09 12:49:49 -0500

    - adding call for worldbank api to get count of scientific journals published by country

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import os
-#import pandas as pd
+import pandas as pd
 from sodapy import Socrata
+import requests
+from xml.etree import ElementTree
 
 nasa_dns = "data.nasa.gov"
 nasa_meteorite_data_set = "y77d-th95"
+
+world_bank_dns="api.worldbank.org"
+world_bank_journal_data_set = "IP.JRN.ARTC.SC"
 
 def main():
     app_token = os.environ["NASA_APP_TOKEN"]
@@ -16,10 +21,14 @@ def main():
         print("Incomplete credentials detected, using unauthorized API calls")
         client = Socrata(nasa_dns, None)
 
-    results = client.get(nasa_meteorite_data_set, limit=2000)
+    results = client.get(nasa_meteorite_data_set)
     print(str(results))
 
     client.close()
+
+    request = requests.get("http://api.worldbank.org/countries/aw/indicators/IP.JRN.ARTC.SC")
+    parsed_response = ElementTree.fromstring(request.content)
+    print(str(parsed_response))
 
 if __name__ == '__main__':
     main()
