d2d55087cea2739ab481366a78d2729f7cedaa34
Rick Pospisil <rick.posp@gmail.com>
2018-06-10 10:17:10 -0500

    - Counting the number of meteor strikes in 2008

--- app.py
+++ app.py
@@ -1,17 +1,36 @@
 import os
 import pandas as pd
 from sodapy import Socrata
-import requests
-from xml.etree import ElementTree
+from datetime import datetime
+import math
+
+pd.set_option('display.max_columns', None)
 
 nasa_dns = "data.nasa.gov"
 nasa_meteorite_data_set = "y77d-th95"
 
-world_bank_dns="api.worldbank.org"
-world_bank_journal_data_set = "IP.JRN.ARTC.SC"
+def convert_string_to_datetime(input_series):
+    retval = None
+    try:
+        retval = datetime.strptime(input_series['year'], '%Y-%m-%dT%H:%M:%S.%f')
+    except Exception as e:
+        print(e)
+        raise e
+    return retval
+
+def strip_nan(input_series):
+    year = input_series['year']
+    if type(year) == float:
+        if math.isnan(year):
+            return False
+    return True
+
+def filter_by_year(input_series, year):
+    date = input_series['year']
+    return date.year == year
 
 def main():
-    app_token = os.environ["NASA_APP_TOKEN"]
+    app_token = os.environ.get("NASA_APP_TOKEN")
 
     client = None
     if app_token != None:
@@ -21,14 +40,21 @@ def main():
         print("Incomplete credentials detected, using unauthorized API calls")
         client = Socrata(nasa_dns, None)
 
-    results = client.get(nasa_meteorite_data_set)
-    print(str(results))
+    results = client.get(nasa_meteorite_data_set, limit=1000000)
 
+    df = pd.DataFrame.from_records(results)
+
+    series = df.apply(strip_nan, axis=1)
+    df = df.loc[series]
+    series = df.apply(convert_string_to_datetime, axis=1)
+    df = df.drop("year", axis=1)
+    df = df.assign(year=series)
+
+    series = df.apply(filter_by_year, axis=1, args=(2008,))
+    df = df.loc[series]
     client.close()
 
-    request = requests.get("http://api.worldbank.org/countries/aw/indicators/IP.JRN.ARTC.SC")
-    parsed_response = ElementTree.fromstring(request.content)
-    print(str(parsed_response))
+    print("Meteor Strikes in 2008: " + str(df.shape[0]))
 
 if __name__ == '__main__':
     main()
